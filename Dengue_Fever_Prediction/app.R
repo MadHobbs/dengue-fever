089076c425f17031755fa66776825ed1d51d6b3f
Madison Hobbs <mhobbs0760@scrippscollege.edu>
2017-12-02 21:38:50 -0800

    can now input data into shiny app!

--- Dengue_Fever_Prediction/app.R
+++ Dengue_Fever_Prediction/app.R
@@ -2,6 +2,8 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
+# Code to read in input to form data frame by Eric Hung, via Stack Overflow.
+#
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
@@ -12,53 +14,97 @@ library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
+  dashboardHeader(title = "Predicting Dengue"),
   ## Sidebar content
   dashboardSidebar(
     sidebarMenu(
       menuItem("Input Predictions For One Week", tabName = "input_one_observation", icon = icon("cog", lib = "glyphicon")),
-      menuItem("Input Data Table", tabName = "input_df", icon = icon("th"))
+      menuItem("Input Data Table", tabName = "input_df", icon = icon("table"))
     )
   ),
   dashboardBody(
     tabItems(
       # First tab content
       tabItem(tabName = "input_one_observation",
+              
               sidebarLayout(
                 
                 sidebarPanel(
+                  
                 sliderInput(inputId = "week_of_year", 
                                              label = "Week of Year (1 to 52)", 
                                              value = 1, min = 1, max = 52), 
                 numericInput(inputId = "avg_temp", 
                                      label = "Week's Average Temperature (degrees C)", 
                                      value = NA),
-                               numericInput(inputId = "total_precip", 
+                numericInput(inputId = "total_precip", 
                                       label = "Week's Total Precipitation (mm)", 
-                                      value = NA)
+                                      value = NA), 
+                hr(),
+                actionButton("addrow", "Add Row"),
+                actionButton("revrow", "Remove Row")
               
               ), 
-              mainPanel()
+              mainPanel(
+                dataTableOutput("table"),
+                hr(),
+                h3("Predictions"),
+                dataTableOutput("prediction_table")
+                
               )
+              )
+              
+              
       ),
       
       # Second tab content
       tabItem(tabName = "input_df",
               # Copy the line below to make a file upload manager
-              fileInput("file", label = h3("Select data file"))
+              fileInput("file", label = h3("Select data file")),
+              dataTableOutput("table_file")
       )
     )
   )
 )
 
-server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
+server <- function(input, output, session) {
   
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
+  values <- reactiveValues()
+  
+  values$DT <- data.frame(week_of_year = NA,
+                          avg_temperature = NA,
+                          total_precipitation = NA,
+                          stringsAsFactors = FALSE)
+  
+  newEntry <- observeEvent(input$addrow, {
+    newLine <- c(input$week_of_year, input$avg_temp, input$total_precip)
+    values$DT <- rbind(values$DT, newLine)
   })
+  
+  newEntry <- observeEvent(input$revrow, {
+    deleteLine <- values$DT[-nrow(values$DT), ]
+    values$DT <- deleteLine
+  })
+  
+  output$table <- renderDataTable({
+    values$DT
+  })
+  
+ # output$print <- renderPrint({ input$week_of_year })
+  
+ # output$table <- renderDataTable({
+    #data <- data.frame(input$week_of_year, input$avg_temp, input$total_precip)
+    #colnames(data) = c("week_of_year", "avg_temp", "total_precip")
+    #data
+   # })
+  #
+ # output$results <- output$table$week_of_year
+  
+  #output$table_file <- renderDataTable(read.csv(input$file))
+  
+  #histdata <- data.frame(input$week_of_year, input$avg_temp, input$total_precip)
+
+ # })
 }
 
 
