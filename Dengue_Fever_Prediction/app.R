212de807da751f8174726e924a048c2758c61f09
Madison Hobbs <mhobbs0760@scrippscollege.edu>
2017-12-02 20:38:13 -0800

    shiny app update

--- Dengue_Fever_Prediction/app.R
+++ Dengue_Fever_Prediction/app.R
@@ -9,50 +9,108 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Use Our Model To Predict Dengue Fever"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-    
-     # sidebar
-      sidebarPanel(
-         numericInput(inputId = "week_of_year", label = "Week of Year (1 to 52)", value = 1, min = 1, max = 52), 
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  ## Sidebar content
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Input Predictions For One Week", tabName = "input_one_observation", icon = icon("cog", lib = "glyphicon")),
+      menuItem("Input Data Table", tabName = "input_df", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "input_one_observation",
+              sidebarLayout(
+                
+                sidebarPanel(
+                sliderInput(inputId = "week_of_year", 
+                                             label = "Week of Year (1 to 52)", 
+                                             value = 1, min = 1, max = 52), 
+                numericInput(inputId = "avg_temp", 
+                                     label = "Week's Average Temperature (degrees C)", 
+                                     value = NA),
+                               numericInput(inputId = "total_precip", 
+                                      label = "Week's Total Precipitation (mm)", 
+                                      value = NA)
+              
+              ), 
+              mainPanel()
+              )
       ),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("scatterPlot")
+      # Second tab content
+      tabItem(tabName = "input_df",
+              # Copy the line below to make a file upload manager
+              fileInput("file", label = h3("Select data file"))
       )
-   )
-   
-   #submitButton()
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
   
-   output$scatterPlot <- renderPlot({
-     hist(rnorm(input$week_of_year))
-   })
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
 }
+
+
+#ui <- fluidPage(
+   
+   # Application title
+   #titlePanel("Use Our Model To Predict Dengue Fever"),
+   #tags$img(height = 100, width = 100, scr = "dengue.jpg"),
+   
+   # Sidebar with a slider input for number of bins 
+   #sidebarLayout(
+    
+     # sidebar
+   #   sidebarPanel(
+    #    
+    #    tags$h3("Input Variables"),
+    #    tags$hr(),
+        
+        # make default values be averages from our data
+     #    sliderInput(inputId = "week_of_year", 
+     #                 label = "Week of Year (1 to 52)", 
+     #                 value = 1, min = 1, max = 52), 
+     #    numericInput(inputId = "avg_temp", 
+              #        label = "Week's Average Temperature (degrees C)", 
+              #        value = NA),
+       #  numericInput(inputId = "total_precip", 
+             #         label = "Week's Total Precipitation (mm)", 
+             #         value = NA),
+        # numericInput(inputId = "total_precip", 
+              #        label = "Week's Total Precipitation (mm)", value = NA),
+         #actionButton(inputId = "go", label = "Submit")
+        
+        
+     # ),
+      
+      # Show a plot of the generated distribution
+     # mainPanel(
+       #  plotOutput("hist")
+     # )
+  # )
+   
+#)
+
+# Define server logic required to draw a histogram
+#server <- function(input, output) {
+  
+  #data <- eventReactive(input$go, {rnorm(input$week_of_year)})
+  
+  # output$hist <- renderPlot({
+   #  hist(data())
+   #})
+#}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
